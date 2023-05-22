23bd46a7434869c4e2d4022841e8f4bea8d3261c
yosrajalali <100087227+yosrajalali@users.noreply.github.com>
2023-05-22 19:30:47 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import Question from './Question';
 import woman from './images/illustration-woman-online-mobile.svg';
 import datas from './data';
-import background from './images/bg-pattern-mobile.svg';
 import deskWoman from './images/illustration-woman-online-desktop.svg';
 import box from './images/illustration-box-desktop.svg';
+import { useState } from 'react';
 
 function App() {
-  const renderedData = datas.map((data, index) => {
+  const [openedBox, setOpenedBox] = useState(null);
+
+  const renderedData = datas.map((data) => {
     return (
       <div key={data.id}>
-        <Question data={data} index={index} />
+        <Question
+          data={data}
+          openedBox={openedBox}
+          setOpenedBox={setOpenedBox}
+        />
         <div className="line"></div>
       </div>
     );
   });
+
   return (
     <main>
       <div className="accordion">
@@ -28,7 +35,7 @@ function App() {
             src={deskWoman}
             alt="woman working with a computer"
           />
-          <img src={box} alt="box image" className="box" />
+          <img src={box} alt="box" className="box" />
         </div>
         <div className="content">
           <h1 className="faq">FAQ</h1>
